85b29bc7093f11985edf157beba0e81da9de1ba4
edwinevi <edwinevi@student.21-school.ru>
2023-05-28 02:23:38 +0300

    create dataclass Event

--- app/src/main/java/com/example/kazan/data/Event.kt
+++ app/src/main/java/com/example/kazan/data/Event.kt
@@ -1,11 +1,17 @@
 package com.example.kazan.data
 
+import com.example.kazan.enums.TypeEvent
 import java.io.Serializable
 
 
 data class Event(
     val image: String,
     val name: String,
+    val address: String,
     val date: String,
-    val address: String
+    val speakers: List<String>,
+    val typeEvent : TypeEvent,
+    val about : String,
+    val webAdress : String,
+    val favorite : Boolean
 ) : Serializable
